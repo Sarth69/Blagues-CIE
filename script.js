7736eaaf3a1ddb210aac267eb013186d3503003c
Romain Bellon <romain.bellon@student-cs.fr>
2022-06-08 14:32:21 +0200

    Adapt random joke to make it inside the onload

--- script.js
+++ script.js
@@ -2,18 +2,6 @@ const blagues = new BlaguesAPI(
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMjYyOTI0NjIyMDc1Mzk2MDk2IiwibGltaXQiOjEwMCwia2V5IjoicTExVHphc1BTT3pzeGU1MkZYNktrWHRjQk13enFTT0JRQ1piajg1VEhzWVI0bmNDMFciLCJjcmVhdGVkX2F0IjoiMjAyMi0wNi0wOFQxMjowODoyOSswMDowMCIsImlhdCI6MTY1NDY5MDEwOX0.Ha2UqzFjl2IeBzbCaP-EBosotzgkzAzW1qjYrT_Utw0"
 );
 
-// Button to fetch a new random joke
-let button = document.getElementById("newRandom");
-button.addEventListener("click", () => {
-  fetchRandomJoke();
-  document
-    .getElementById("randomJoke")
-    .setAttribute("class", "accordion-button collapsed custom-bg");
-  document
-    .getElementById("collapseOne")
-    .setAttribute("class", "accordion-collapse collapse");
-});
-
 let fetchRandomJoke = async () => {
   let blague = await blagues.random();
   jokeElement = document.getElementById("randomJoke");
@@ -22,4 +10,18 @@ let fetchRandomJoke = async () => {
   answerElement.innerText = blague.answer;
 };
 
-fetchRandomJoke();
+let onRandomLoad = () => {
+  // Button to fetch a new random joke
+  let button = document.getElementById("newRandom");
+  button.addEventListener("click", () => {
+    fetchRandomJoke();
+    document
+      .getElementById("randomJoke")
+      .setAttribute("class", "accordion-button collapsed custom-bg");
+    document
+      .getElementById("collapseOne")
+      .setAttribute("class", "accordion-collapse collapse");
+  });
+};
+
+// fetchRandomJoke();
