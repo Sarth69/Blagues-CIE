96f5455774847ccee970495fd8eec0eeff1ef4a9
Bouthillon Louis <louis.bouthillon@student-cs.fr>
2022-06-09 12:24:24 +0200

    route_set

---
+++ server/index.js
@@ -0,0 +1,18 @@
+var express = require('express'); //import de la bibliothèque Express
+var app = express(); //instanciation d'une application Express
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// setting up cross-origin policy
+const cors = require('cors');
+app.use(cors({
+    origin: 'file:///C:/Users/louis/Blagues-CIE'
+}));
+
+app.get('/like',(req,res) => {
+    res.send("un like de plus !");
+});
+
+app.listen(8080); //commence à accepter les requêtes
+console.log("App listening on port 8080...");
